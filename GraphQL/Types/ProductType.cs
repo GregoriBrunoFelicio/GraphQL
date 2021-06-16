bda9ba79dfbb3fad15d53028fea0027d5f17f499
gregori bruno felicio <gregory.wow@hotmail.com>
2021-06-15 21:20:43 -0300

    create product type

---
+++ GraphQL/Types/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Models;
+using GraphQL.Types;
+
+namespace GraphQL.Project.GraphQL.Types
+{
+    public sealed class ProductType : ObjectGraphType<Product>
+    {
+        public ProductType()
+        {
+            Field(p => p.Id);
+            Field(p => p.Name);
+            Field(p => p.Price);
+        }
+    }
+}
