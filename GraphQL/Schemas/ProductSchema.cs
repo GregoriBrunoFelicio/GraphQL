b3db371494da3f5d79809aa6e2984c8482ffbcdc
gregori bruno felicio <gregory.wow@hotmail.com>
2021-06-16 20:13:20 -0300

    create product shcema

---
+++ GraphQL/Schemas/ProductSchema.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Project.GraphQL.Queries;
+using GraphQL.Types;
+
+namespace GraphQL.Project.GraphQL.Schemas
+{
+    public class ProductSchema : Schema
+    {
+        public ProductSchema(ProductQueries productQueries)
+        {
+
+            Query = productQueries;
+        }
+    }
+}
