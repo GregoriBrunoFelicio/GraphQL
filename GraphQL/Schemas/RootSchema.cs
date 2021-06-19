aac6843a07269f56174ed44d21e3e23a90d4365a
gregori bruno felicio <gregory.wow@hotmail.com>
2021-06-19 14:29:38 -0300

    create root query and create root schema

---
+++ GraphQL/Schemas/RootSchema.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Project.GraphQL.Queries;
+using GraphQL.Types;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace GraphQL.Project.GraphQL.Schemas
+{
+    public class RootSchema : Schema
+    {
+        public RootSchema(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            Query = serviceProvider.GetRequiredService<RootQuery>();
+        }
+    }
+}
